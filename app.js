88cf6b430b36ec59984b69296e5973dd27cff72f
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2024-12-10 17:22:56 +0530

    set directory of files
    set directory of files

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// import express from 'express';
+// import cookieParser from 'cookie-parser';
+
+// const app = express()
+// app.use(express.json({limit: "16kb"}))
+// app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// app.use(express.static("public"))
+// app.use(cookieParser());
+
+
+
+// //routes import
+// import userRouter from './src/routes/user.routes.js';
+// import customerRouter from './src/routes/customer.routes.js';
+// import driverRouter from './src/routes/driver.routes.js';
+// import partnerRouter from './src/routes/partner.routes.js';
+// import carRouter from './src/routes/car.routes.js'
+
+// app.post("/registerDriver", (req, res) => {
+//     console.log(req.body); // Log non-file fields
+//     console.log(req.files); // Log uploaded files
+// });
+
+
+// //routes declaration
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/customers",customerRouter);
+// app.use("/api/v1/drivers",driverRouter);
+// app.use("/api/v1/partners",partnerRouter);
+// app.use("/api/v1/cars",carRouter)
+
+// export { app }
+
+
+// // check push request
--- config/db.js
+++ config/db.js
@@ -1,31 +1,14 @@
-// config/db.js
 import mongoose from 'mongoose';
-import {app} from '../app.js';
-
-
-
-
 
 const connectDB = async () => {
   try {
     const dbURI = process.env.MONGO_URI; // Fetch the Mongo URI from the environment variable
-
-    
     await mongoose.connect(dbURI);
     console.log('MongoDB connected');
-    
-    const port = process.env.PORT || 3000;
-
-    
-    // Start the server after the DB connection is established
-    app.listen(port, '0.0.0.0', () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
-
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1); // Exit the process if the database connection fails
   }
 };
 
-export {connectDB}
+export { connectDB };
