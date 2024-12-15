a7b73635cd4da2af9a350f1379ccd14fa44222b2
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2024-12-15 02:36:00 -0800

    Add car router of fetch car details by userId
    Add car router of fetch car details by userId

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -2,7 +2,7 @@ import {Router} from 'express';
 import {Car} from '../models/car.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js'; 
 import { multerUpload } from '../middlewares/multerService.js';
-import { addCar,deleteCar,getAllCars, getCarById,updateCarDetails } from '../controllers/car.controller.js';
+import { addCar,deleteCar,getAllCars, getCarById, getCarByUserId,updateCarDetails } from '../controllers/car.controller.js';
 
 
 const router = Router();
@@ -23,6 +23,7 @@ router.post('/addCar',authMiddleware,multerUpload.fields(
 
 router.get('/getAllCars', getAllCars)
 router.get('/getCarByid/:carId', getCarById)
+router.get('/getCarByUserId', authMiddleware, getCarByUserId)
 router.put('/updateCar/:carId',authMiddleware, updateCarDetails)
 router.delete('/deletaCar/:carId', deleteCar)
 
