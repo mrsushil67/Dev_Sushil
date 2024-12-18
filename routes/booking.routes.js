4e9be48a686e773fd5b223d43290fb36dfb53448
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2024-12-18 18:20:13 +0530

    Add route of GetBookingByDriverId
    Add route of GetBookingByDriverId

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -2,7 +2,7 @@ import {Router} from 'express';
 import {Car} from '../models/car.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js'; 
 import { multerUpload } from '../middlewares/multerService.js';
-import { createBooking,getAllBooking, getBookingByCarId, getBookingByUserId, updateBookingPaymentStatus} from '../controllers/booking.controller.js';
+import { createBooking,getAllBooking, getBookingByCarId, getBookingByUserId, getBookingByDriverId, deleteBookingById, updateBookingPaymentStatus} from '../controllers/booking.controller.js';
 
 const router = Router();
 
@@ -10,6 +10,8 @@ router.post('/createBooking',createBooking)
 router.get("/getAllBooking",getAllBooking)
 router.get('/getBookingByCarId/:carId',authMiddleware,getBookingByCarId)
 router.get("/getBookingByuserId", authMiddleware, getBookingByUserId)
+router.get("/getBookingBydriverId", authMiddleware, getBookingByDriverId)
+router.delete("/delete/:driverId", deleteBookingById)
 router.put(`/paymentstatus`,updateBookingPaymentStatus)
 
 export default router;
